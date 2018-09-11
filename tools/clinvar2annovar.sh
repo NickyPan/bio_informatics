ce1716a82d711ad4e9653d32b6855721069217ae
Nicky Pan <pxc333@163.com>
2018-09-11 17:31:35 +0800

    script link

--- tools/clinvar2annovar.sh
+++ tools/clinvar2annovar.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ANNOVAR=/opt/seqtools/annovar
+TOOL_DIR=/mnt/workshop/xinchen.pan/bio_informatics/tools
+
 if [ "$2" = "down" ];then
     wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/$1
     wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/$1.md5
@@ -12,12 +15,12 @@ if md5sum -c $1.md5; then
     filename='hg19_'$suffix_name'.txt'
     echo "$suffix_name"
     vt decompose $1 -o temp.split.vcf
-    perl prepare_annovar_user.pl -dbtype clinvar_preprocess2 temp.split.vcf -out temp.split2.vcf
+    perl $ANNOVAR/prepare_annovar_user.pl -dbtype clinvar_preprocess2 temp.split.vcf -out temp.split2.vcf
     sed 's/^/chr/' temp.split2.vcf | sed 's/chr#/#/' - | sed 's/##contig=<ID=/##contig=<ID=chr/' - > temp.split3.vcf
     sed -i '/chrNW/d' temp.split3.vcf
     vt normalize -n temp.split3.vcf -r hg19.fa -o temp.norm.vcf -w 2000000
-    perl prepare_annovar_user.pl -dbtype clinvar2 temp.norm.vcf -out $fileRaw
-    python mod_annovar.py -i $fileRaw -o $filename --dtype clinvar
+    perl $ANNOVAR/prepare_annovar_user.pl -dbtype clinvar2 temp.norm.vcf -out $fileRaw
+    python $TOOL_DIR/mod_annovar.py -i $fileRaw -o $filename --dtype clinvar
     rm -f temp.* $fileRaw
     # index_annovar.pl temp.norm.txt -out $filename -comment comment_temp.txt
 else
